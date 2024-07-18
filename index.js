6a999b22070c2a46da0ddfa7b7cdf087ff67bd5b
Monika <laiskutis.monikai@gmail.com>
2024-07-18 14:54:48 +0300

    oop

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-import { dog } from './js/Dog.js';
+import { Dog } from "./js/Dog.js";
 
-dog();
-dog();
-dog();
-dog();
-dog();
-dog();
-dog();
+const rexas = new Dog('Rexas', 'black', 3);
+console.log(rexas);
+
+console.log(rexas.hi());
+console.log(rexas.think());
+console.log(rexas.addNumbers(3, 2));
+console.log(rexas.addNumbers(3, -2));
+console.log(rexas.addNumbers(-3, -2));
+console.log(rexas.addNumbers(-3, 3));
+console.log(rexas.manyBones(20));
+
+console.clear();
+
+const brisius = new Dog('Brisius', 'white', 2);
+console.log(brisius);
+
+console.log(brisius.lostLeg());
+console.log(brisius.lostLeg());
+console.log(brisius.lostLeg());
+console.log(brisius.lostLeg());
+console.log(brisius.lostLeg());
+console.log(brisius.lostLeg());
+console.log(brisius.lostLeg());
+
+console.log(brisius.birthday());
+console.log(brisius.birthday());
+console.log(brisius.birthday());
---
+++ js/auto-nd.md
@@ -0,0 +1,21 @@
+class -> Car (Automobilis)
+
+-   pavadinimas (Audi)
+-   modelis (80)
+-   color
+-   kuro bako talpa (litrais)
+-   vidutines kuro sanaudos 100km
+-   ar ijungtas varyklis (default: false)
+-   greitis (default: 0)
+
+metodai:
+
+-   ijungti varykli
+    -   ijungto varykli dar karto ijungti negalima, sugadinti starteri
+-   isjungti varykli
+    -   isjungto varyklio isjungti negalima...
+-   pradeti vaziuoti (greitis tiesiog tampa ne nulinis ir sunaudoja 2x litrus kuro sanaudu)
+-   vaziuoti (naudoja 1x litro kuro sanaudu)
+-   sustoti (greitis tiesiog tampa nulinis)
+-   kiek liko kuro?
+-   uzpilti kuro baka (kiek litrais)
